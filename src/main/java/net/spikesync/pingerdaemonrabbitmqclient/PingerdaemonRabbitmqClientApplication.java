2e5d45114c1ed1d4ead01157945a562f823a3b9e
Fred Wan <mail@fredwan.net>
2023-09-19 19:07:51 +0200

    Updated PingerdaemonRabbitmqClientApplicationTests to read the variable test-pingerdaemon-context from a properties file and determine to execute the test based on its value (or its absence). Using Junit 5 annotation @EnabledIf

--- src/main/java/net/spikesync/pingerdaemonrabbitmqclient/PingerdaemonRabbitmqClientApplication.java
+++ src/main/java/net/spikesync/pingerdaemonrabbitmqclient/PingerdaemonRabbitmqClientApplication.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
