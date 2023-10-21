5ce124e96f52d4076041ced7f784942e355b5635
Fred Wan <mail@fredwan.net>
2023-10-21 16:56:22 +0200

    The REST endpoint is running with Spring Boot, but it doesn't return the current pingHeatMap. TBD: debug and fix.

--- src/main/java/net/spikesync/pingerdaemonrabbitmqclient/PingerdaemonRabbitmqClientApplication.java
+++ src/main/java/net/spikesync/pingerdaemonrabbitmqclient/PingerdaemonRabbitmqClientApplication.java
@@ -2,6 +2,7 @@ package net.spikesync.pingerdaemonrabbitmqclient;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Properties;
 
@@ -9,21 +10,44 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import net.spikesync.api.PingHeatMapController;
 
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ImportResource(locations = {"classpath:beans.xml"})
+@SpringBootApplication
 public class PingerdaemonRabbitmqClientApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(PingerdaemonRabbitmqClientApplication.class);
 	private ApplicationContext context = new GenericXmlApplicationContext("classpath:beans.xml");
 	private PingMsgReader pingMsgReader;
 	private PingHeatMap pingHeatMap;
+	private PingHeatMapController pingHeatMapController;
     
+	
+	public PingerdaemonRabbitmqClientApplication(PingMsgReader piMsRe, PingHeatMap piHeMa, PingHeatMapController piHeMaCo) {
+	   	this.pingMsgReader = piMsRe;
+    	this.pingHeatMap = piHeMa;
+    	this.pingHeatMapController = piHeMaCo;
+
+	}
+	
     public static void main(String[] args) {
-    	PingerdaemonRabbitmqClientApplication devPingApp = new PingerdaemonRabbitmqClientApplication();
-    	devPingApp.pingMsgReader = devPingApp.context.getBean(PingMsgReader.class);
+    	SpringApplication  springDevPingApp = new SpringApplication(PingerdaemonRabbitmqClientApplication.class);
+    	PingerdaemonRabbitmqClientApplication devPingApp = new PingerdaemonRabbitmqClientApplication(null,null,null);
+       	devPingApp.pingMsgReader = devPingApp.context.getBean(PingMsgReader.class);
     	devPingApp.pingHeatMap = devPingApp.context.getBean(PingHeatMap.class);
+
+    	springDevPingApp.setDefaultProperties(Collections.singletonMap("server.port", "8098"));
+    	springDevPingApp.run(args);
     	
     	
  		Properties prop = PropertiesLoader.loadProperties();
