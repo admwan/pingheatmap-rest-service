9d92b5185594b5771e2c0322b5999c71320fe8ca
Fred Wan <mail@fredwan.net>
2023-10-21 19:37:54 +0200

    Quickly added a method to check for calls to PingHeatMap.getLastSuccessfulPing(...). Returns null. TBD: find out what's going on. 	@Autowired 	@PostMapping(/lastcaptpingdate) 	public Date getLastCaptPingUpdate() {  pingHeatMap.getLastTimeSuccesfulPing(... ); ... }

--- src/main/java/net/spikesync/api/PingHeatMapController.java
+++ src/main/java/net/spikesync/api/PingHeatMapController.java
@@ -1,16 +1,11 @@
 package net.spikesync.api;
 
+import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +22,7 @@ public class PingHeatMapController {
 
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(PingHeatMapController.class);
 
+	@Autowired
 	private PingHeatMap pingHeatMap;
 
 	public PingHeatMapController(PingHeatMap piHeMa) {
@@ -34,9 +30,20 @@ public class PingHeatMapController {
 		
 	}
 	
+	@Autowired
 	@PostMapping("/pingheatmap")
 	public HashMap<SilverCloudNode, HashMap<SilverCloudNode, PingHeatData>> getPingHeatMap() {
 		logger.debug("Now returning pingHeatMap as HashMap from REST API method getPingHeatMap");
-		return this.pingHeatMap.getPingHeatmap();
+		return pingHeatMap.getPingHeatmap();
+	}
+	
+	@Autowired
+	@PostMapping("/lastcaptpingdate")
+	public Date getLastCaptPingUpdate() {
+		SilverCloudNode captNode = new SilverCloudNode("CAPTUW","192.168.50.104");
+		SilverCloudNode thorNode = new SilverCloudNode("THORFW","192.168.50.107");
+		Date lastCaptThorPing =  pingHeatMap.getLastTimeSuccesfulPing(captNode, thorNode);
+		logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Date of the last CAPTUW-THORFW successful ping that should be returned is: " + lastCaptThorPing);
+		return lastCaptThorPing;
 	}
 }
