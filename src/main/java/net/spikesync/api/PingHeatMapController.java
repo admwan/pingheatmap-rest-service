2060bc1f73dcc24f68821e4b292db39c2f50d623
Fred Wan <mail@fredwan.net>
2023-10-21 18:07:07 +0200

    Something went wrong with the last commit and subsequent merge in main. Retrying

--- src/main/java/net/spikesync/api/PingHeatMapController.java
+++ src/main/java/net/spikesync/api/PingHeatMapController.java
@@ -4,19 +4,25 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.spikesync.pingerdaemonrabbitmqclient.PingHeatData;
 import net.spikesync.pingerdaemonrabbitmqclient.PingHeatMap;
 import net.spikesync.pingerdaemonrabbitmqclient.SilverCloudNode;
 
+
+
 @RestController
+@RequestMapping(path="/", produces="application/json")
+@CrossOrigin(origins="http://localhost:8098")
 public class PingHeatMapController {
 
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(PingHeatMapController.class);
