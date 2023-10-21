4d32d4e4267fbd64e2d028b5b1042348a6816d37
Fred Wan <mail@fredwan.net>
2023-10-21 14:54:11 +0200

    Added class PingHeatMapController (REST method) and the Bean configuration for it in beans.xml

---
+++ src/main/java/net/spikesync/api/PingHeatMapController.java
@@ -0,0 +1,36 @@
+package net.spikesync.api;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.spikesync.pingerdaemonrabbitmqclient.PingHeatData;
+import net.spikesync.pingerdaemonrabbitmqclient.PingHeatMap;
+import net.spikesync.pingerdaemonrabbitmqclient.SilverCloudNode;
+
+@RestController
+public class PingHeatMapController {
+
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(PingHeatMapController.class);
+
+	private PingHeatMap pingHeatMap;
+
+	public PingHeatMapController(PingHeatMap piHeMa) {
+		pingHeatMap = piHeMa;
+		
+	}
+	
+	@PostMapping("/pingheatmap")
+	public HashMap<SilverCloudNode, HashMap<SilverCloudNode, PingHeatData>> getPingHeatMap() {
+		logger.debug("Now returning pingHeatMap as HashMap from REST API method getPingHeatMap");
+		return this.pingHeatMap.getPingHeatmap();
+	}
+}
